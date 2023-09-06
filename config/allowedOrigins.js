032e853f820151ce3814fecabbfda4808b9a205d
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2023-09-06 07:57:12 +0530

    Added a new cor

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins=[
     "http:127.0.0.1:5500",
     "http://localhost:3500",
-    "https://www.google.com"
+    "https://www.google.com",
+    "http://www.mysite.lk"
 ]
 
 module.exports=allowedOrigins
